2d255dce6c5f5c3aaef95cc18be90a44124f0b06
m-ghiringhelli <mghiring@gmail.com>
2022-02-22 14:20:28 -0800

    dynamically generate select menu content

--- enroll/enroll.js
+++ enroll/enroll.js
@@ -1,5 +1,22 @@
-import { checkAuth } from '../fetch-utils.js';
+import { checkAuth, logout, getWorkshops } from '../fetch-utils.js';
 
 const enrollForm = document.getElementById('enroll-form');
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    console.log('clicked');
+});
+
+window.addEventListener('load', async () => {
+    const selectEl = document.getElementById('workshops');
+    const workshops = await getWorkshops();
+    for (let workshop of workshops) {
+        const option = document.createElement('option');
+        option.value = workshop.id;
+        option.textContent = workshop.name;
+        selectEl.append(option);
+    }
+});
 
 checkAuth();
+
